592676e1ca9853665c2ce046907da21c69cb0dde
Himanshi Singh <72205663+himanshi-singh-jadaun@users.noreply.github.com>
2023-06-08 19:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ app.use("/api/pins", pinRoute);
 //     console.log("Backend server is running");
 // })
 
-app.listen(443,[100.20.92.101,
-44.225.181.72,
-44.227.217.144], () => {
+app.listen(443, 100.20.92.101, () => {
     console.log("Backend server is running");
 })
